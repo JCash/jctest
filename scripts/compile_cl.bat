3190c4b93f2939ea45aaccbfe8a87232c32649a2
JCash <mwesterdahl76@gmail.com>
2019-03-05 00:05:39 +0100

    Added latest tests to win32

--- scripts/compile_cl.bat
+++ scripts/compile_cl.bat
@@ -45,6 +45,8 @@ mkdir build
 
 cl.exe /O2 /D_CRT_SECURE_NO_WARNINGS /D_HAS_EXCEPTIONS=0 /EHsc /W4 /Isrc test/test_params.cpp test/main.cpp /link /out:.\build\test_params.exe
 cl.exe /O2 /D_CRT_SECURE_NO_WARNINGS /D_HAS_EXCEPTIONS=0 /EHsc /W4 /Isrc test/test_typed_test.cpp test/main.cpp /link /out:.\build\test_typed_test.exe
+cl.exe /O2 /D_CRT_SECURE_NO_WARNINGS /D_HAS_EXCEPTIONS=0 /EHsc /W4 /Isrc test/test_expect.cpp test/main.cpp /link /out:.\build\test_expect.exe
+cl.exe /O2 /D_CRT_SECURE_NO_WARNINGS /D_HAS_EXCEPTIONS=0 /EHsc /W4 /Isrc test/test_death.cpp test/main.cpp /link /out:.\build\test_death.exe
 
 del *.obj
 
--- scripts/run_tests.bat
+++ scripts/run_tests.bat
@@ -2,4 +2,6 @@ echo off
 
 .\build\test_params.exe
 .\build\test_typed_test.exe
+.\build\test_expect.exe
+.\build\test_death.exe
 
