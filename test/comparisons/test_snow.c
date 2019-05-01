b6000881b1a0d7861f2f6fcd48ff3f67090396d3
JCash <mwesterdahl76@gmail.com>
2019-05-01 13:02:33 +0200

    Fix

--- test/comparisons/test_snow.c
+++ test/comparisons/test_snow.c
@@ -1,25 +1,13 @@
-#include "greatest.h"
+#include <snow/snow.h>
 
-// Add definitions that need to be in the test runner's main file.
-GREATEST_MAIN_DEFS();
+describe(operations) {
+    it("multiplication") {
+        asserteq_int(4, 2*2);
+    }
 
-TEST mytest_multiplication(void) {
-    ASSERT_EQ(4, 2*2);
-    PASS();
+    it("division") {
+        asserteq_int(2, 4/2);
+    }
 }
 
-TEST mytest_division(void) {
-    ASSERT_EQ(2, 4/2);
-    PASS();
-}
-
-SUITE(suite) {
-    RUN_TEST(mytest_multiplication);
-    RUN_TEST(mytest_division);
-}
-
-int main(int argc, char **argv) {
-    GREATEST_MAIN_BEGIN();
-    RUN_SUITE(suite);
-    GREATEST_MAIN_END();
-}
+snow_main();
