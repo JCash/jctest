ef4cca607c923236423443e77d46606a96660d8d
JCash <mwesterdahl76@gmail.com>
2019-11-10 11:54:07 +0100

    Added ASSERT_ARRAY_EQ and ASSERT_ARRAY_EQ_LEN Added JC_TEST_MEMCMP macro for memcmp

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -72,6 +72,7 @@ time compile_test typed_test
 time compile_test expect
 time compile_test death
 time compile_test empty
+time compile_test array
 
 if [ "$TRAVIS_COMPILER" == "" ]; then
     echo ""
